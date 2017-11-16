f7ae78d191400940dbc637da95f064bb7c8b1630
Ikey Doherty <ikey@solus-project.com>
2017-11-16 02:41:50 +0000

    Add initial dummy binary (does nothing useful)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/main.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of usysconf.
+ *
+ * Copyright © 2017 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+        fprintf(stderr, "Not yet implemented\n");
+        return EXIT_FAILURE;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
