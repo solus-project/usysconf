1dc0324a203c9b5323810c37e5fd5d6b376fed74
Ikey Doherty <ikey@solus-project.com>
2017-11-29 12:52:48 +0000

    Add `mkrelease.sh` and set version to 0.0.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ mkrelease.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+git submodule init
+git submodule update
+
+VERSION="0.0.1"
+NAME="usysconf"
+git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+xz -9 "${NAME}-${VERSION}.tar"
+
+gpg --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"               
