acd6d7a01b3d065f894c0de423705a412e9fcc58
Ikey Doherty <ikey@solus-project.com>
2018-02-08 11:32:00 +0000

    Bump version to 0.5.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="0.5.0"
+VERSION="0.5.1"
 NAME="usysconf"
 git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
